02776ca9e99915f9218f6f92cb9742f3f19beda1
suhana <suhanapks@gmail.com>
2025-04-01 04:32:37 +0530

    fourth commit

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ connectDB();
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(
+  cors({
+    origin: "https://e-commerce-client-rayan.onrender.com", // Allow only your frontend domain
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
 //app.use(cors({origin:"http://localhost:5173",credentials:true,methods:["GET","POST","PUT","DELETE","OPTION"]}));
 console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 console.log("JWT_SECRET_KEY from .env:", process.env.JWT_SECRET_KEY);
