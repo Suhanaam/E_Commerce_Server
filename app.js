b8e6fee4ab1bb796dc0cb47ce20d07587376aaa7
suhana <suhanapks@gmail.com>
2025-04-01 04:09:25 +0530

    third commit

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ connectDB();
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",credentials:true,methods:["GET","POST","PUT","DELETE","OPTION"]}));
+app.use(cors())
+//app.use(cors({origin:"http://localhost:5173",credentials:true,methods:["GET","POST","PUT","DELETE","OPTION"]}));
 console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 console.log("JWT_SECRET_KEY from .env:", process.env.JWT_SECRET_KEY);
 
