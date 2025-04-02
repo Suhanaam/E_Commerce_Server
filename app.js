e7a1194c1f65f51a51086a5c32809057897c56e2
suhana <suhanapks@gmail.com>
2025-04-03 01:03:02 +0530

    sixth commit

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ app.use(cookieParser());
 console.log("MONGO_URI from .env:", process.env.MONGO_URI);
 console.log("JWT_SECRET_KEY from .env:", process.env.JWT_SECRET_KEY);
 
+app.use(express.urlencoded({ extended: true }));
+
+// Explicitly allow credentials in every response
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://e-commerce-client-rayan.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
